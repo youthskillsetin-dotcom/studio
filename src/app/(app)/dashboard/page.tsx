d07a2a3931af69ac25d8efa233bf89c5343c736c
yash <yashneetkundal@gmail.com>
2025-09-06 12:33:15 +0000

    Try fixing this error: `Runtime Error: TypeError: Super expression must

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
