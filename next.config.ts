8b6907f9b39411f04be2001be902c745f8516c2a
Youth Skill <youthskillset.in@gmail.com>
2025-09-07 05:38:08 +0000

    TypeError: Invalid URL
    src/middleware.ts (13:37) @ middleware
    
      11 |

--- next.config.ts
+++ next.config.ts
@@ -32,10 +32,6 @@ const nextConfig: NextConfig = {
       '*.firebase.studio',
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }
 };
 
 export default nextConfig;
