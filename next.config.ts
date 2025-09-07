8f33b3c2f5551a9b51e4ad9703712850e60fd508
Youth Skill <youthskillset.in@gmail.com>
2025-09-07 05:40:00 +0000

    is that nesssory

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,10 @@ const nextConfig: NextConfig = {
       '*.firebase.studio',
     ],
   },
+   env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 export default nextConfig;
