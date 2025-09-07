1f4622a43d8552c72069be4cc06ef1f44bc85a83
yash <yashneetkundal@gmail.com>
2025-09-07 08:33:53 +0000

    rearrange this\ (_for element <Footer>_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -48,6 +48,18 @@ export function Footer() {
            </div>
            
           <div className="md:mx-auto">
+            <h3 className="font-semibold font-headline text-foreground tracking-wider uppercase">Follow Us</h3>
+             <div className="flex justify-center md:justify-start mt-4 space-x-4">
+                {socialLinks.map((item, idx) => (
+                     <Link key={idx} href={item.href} className="text-muted-foreground hover:text-primary transition-colors duration-300">
+                        <item.icon className="w-6 h-6" />
+                        <span className="sr-only">{item.name}</span>
+                    </Link>
+                ))}
+            </div>
+          </div>
+          
+           <div className="md:ml-auto">
              <h3 className="font-semibold font-headline text-foreground tracking-wider uppercase">Navigation</h3>
              <ul className="mt-4 space-y-2">
                 {footerNavs.map((item, idx) => (
@@ -58,18 +70,6 @@ export function Footer() {
                     </li>
                 ))}
              </ul>
-          </div>
-          
-           <div className="flex flex-col items-center md:items-end">
-             <h3 className="font-semibold font-headline text-foreground tracking-wider uppercase">Follow Us</h3>
-             <div className="flex mt-4 space-x-4">
-                {socialLinks.map((item, idx) => (
-                     <Link key={idx} href={item.href} className="text-muted-foreground hover:text-primary transition-colors duration-300">
-                        <item.icon className="w-6 h-6" />
-                        <span className="sr-only">{item.name}</span>
-                    </Link>
-                ))}
-            </div>
            </div>
         </div>
         
