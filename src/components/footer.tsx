84f7fea35328f3dfba3aed8adafa52cd92dafc80
yash <yashneetkundal@gmail.com>
2025-09-09 15:40:13 +0000

    add the good whatsapp logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,12 +3,12 @@
 
 import {
   Instagram,
-  MessageSquare,
   Twitter,
 } from 'lucide-react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { Logo } from './icons';
+import type { SVGProps } from 'react';
 
 const supportLinks = [
     { href: '/about', name: 'About' },
@@ -19,10 +19,29 @@ const supportLinks = [
     { href: '/refund-policy', name: 'Refund Policy' },
 ]
 
+function WhatsappIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+      </svg>
+    )
+  }
+
 const socialLinks = [
     { href: 'https://x.com/YouthSkillSet', icon: Twitter, name: 'Twitter' },
     { href: 'https://www.instagram.com/youthskillset/', icon: Instagram, name: 'Instagram' },
-    { href: 'https://chat.whatsapp.com/LyQqKZ6w2Y77kZHHwKmais?mode=ems_copy_c', icon: MessageSquare, name: 'WhatsApp' },
+    { href: 'https://chat.whatsapp.com/LyQqKZ6w2Y77kZHHwKmais?mode=ems_copy_c', icon: WhatsappIcon, name: 'WhatsApp' },
 ];
 
 export function Footer() {
