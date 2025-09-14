516b1306318c872485a7d83615e1a4770ddd1a2a
Youth Skill <youthskillset.in@gmail.com>
2025-09-14 06:31:16 +0000

    make suire everything work properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,52 +1,15 @@
+/** @type {import('next').NextConfig} */
 
-import { config } from 'dotenv';
-config();
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
-import withPWA from 'next-pwa';
-
-const isDev = process.env.NODE_ENV === 'development';
-
-const pwaConfig = withPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: isDev,
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  // any other next.js config
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/sitemap',
-        destination: '/sitemap.xml',
-        permanent: true,
-      },
-    ]
-  },
-};
-
-export default pwaConfig(nextConfig);
+module.exports = nextConfig;
